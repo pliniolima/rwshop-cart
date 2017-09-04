7374e583e73f543ac0486e2d550a28b881e474ed
pliniolima <pliniolifeson@gmail.com>
2017-09-04 17:22:37 -0400

    ecommerce

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var products = Product.find(function (err, docs) {
+		var chunks = [];
+		var size = 3;
+		for (var i=0; i<docs.length; i+=size)
+			chunks.push(docs.slice(i, i + size));
+		console.log(chunks.length);
+		res.render('shop/index', { title: 'Shopping Cart', products: chunks });		
+	});
 });
 
 module.exports = router;
